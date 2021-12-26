6d71885d2493e4273af77455878ae9188903b030
Elias Rogov <eliasrogov@gmail.com>
2021-12-26 17:55:26 +0100

    fix with flyway

--- src/main/resources/db/migration/V2__add_admin.sql
+++
@@ -1,4 +0,0 @@
-insert into users (id, archive, email, name, password, role, bucket_id)
-values (1, false, 'mail@mail.de', 'admin', 'pass', 'ADMIN', null);
-
-alter sequence  user_seq restart  with 2;
