c09b0d9f217a61de6b8e2ea0759a9efe145b4117
Elias Rogov <eliasrogov@gmail.com>
2021-12-26 15:28:00 +0100

    added allocationSize = 1

--- src/main/java/htw/webtech/smarthome/domain/Product.java
+++ src/main/java/htw/webtech/smarthome/domain/Product.java
@@ -20,7 +20,7 @@ public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = SEQ_NAME)
-    @SequenceGenerator(name = SEQ_NAME, sequenceName = SEQ_NAME)
+    @SequenceGenerator(name = SEQ_NAME, sequenceName = SEQ_NAME, allocationSize = 1)
     private Long id;
     private String title;
     private BigDecimal price;
