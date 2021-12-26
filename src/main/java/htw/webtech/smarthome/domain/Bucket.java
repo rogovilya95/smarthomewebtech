d1339018b7e2064cad82a30c7371ac08b9ae046d
Elias Rogov <eliasrogov@gmail.com>
2021-12-26 14:51:40 +0100

    created new project

---
+++ src/main/java/htw/webtech/smarthome/domain/Bucket.java
@@ -0,0 +1,24 @@
+package htw.webtech.smarthome.domain;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "buckets")
+
+public class Bucket {
+    private static final String SEQ_NAME = "bucket_seq";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = SEQ_NAME)
+    @SequenceGenerator(name = SEQ_NAME, sequenceName = SEQ_NAME)
+    private Long id;
+    
+}
---
+++ src/main/java/htw/webtech/smarthome/domain/Role.java
@@ -0,0 +1,5 @@
+package htw.webtech.smarthome.domain;
+
+public enum Role {
+    CLIENT, MANAGER, ADMIN
+}
