f1f786470e408af8b6a6523e50f9533ffd99aa4d
Elias Rogov <eliasrogov@gmail.com>
2022-01-12 09:08:03 +0100

    'fixed user entity'

--- src/main/java/htw/webtech/smarthome/domain/AuthenticationToken.java
+++ src/main/java/htw/webtech/smarthome/domain/AuthenticationToken.java
@@ -10,7 +10,7 @@ import java.util.UUID;
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         @Column(name = "id")
-        private Long id;
+        private long id;
 
         private String token;
 
